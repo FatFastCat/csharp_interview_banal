ddbffba30a6ec6b7fdf1fd4ea7a64473977dde95
FatFastCat <cy.r1.shevtsov@gmail.com>
2023-05-24 22:31:24 +0300

    added FizzBuzz sln vs code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿string word;
+
+for (int i = 1; i < 101; i++)
+{
+    word = i.ToString();
+    if (i % 15 == 0)
+        word += " FizzBazz";
+    else if (i % 5 == 0)
+        word += " Buzz";
+    else if (i % 3 == 0)
+        word += " Fizz";
+    Console.WriteLine($"{word}");
+}
