50759684410572b8fce2969d2fd356fc64116bdf
FatFastCat <cy.r1.shevtsov@gmail.com>
2023-05-25 10:49:34 +0300

    Added do-while example

---
+++ DoWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+===============
+E X E R C I S E
+===============
+Here are the rules for the battle game that you need to implement in your code project:
+- You must use either the do-while statement or the while statement as an outer game loop.
+- The hero and the monster will start with 10 health points.
+- All attacks will be a value between 1 and 10.
+- The hero will attack first.
+- Print the amount of health the monster lost and their remaining health.
+- If the monster's health is greater than 0, it can attack the hero.
+- Print the amount of health the hero lost and their remaining health.
+- Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+- Print the winner.
+===============
+By MS Learn: 
+https://learn.microsoft.com/en-us/training/modules/csharp-do-while/3-challenge
+===============
+*/
+
+
+var heroHealth = 10;
+var monsterHealth = 10;
+
+Random rnd = new Random();
+
+do
+{
+    var hitDamage = rnd.Next(1,11);
+    monsterHealth -= hitDamage;
+    Console.WriteLine($"Monster was damaged and lost {hitDamage} health and now has {monsterHealth} health.");
+
+    if (monsterHealth <= 0) continue;
+
+    hitDamage = rnd.Next(1, 11);
+    heroHealth -= hitDamage;
+    Console.WriteLine($"Her was damaged and lost {hitDamage} health and now has {heroHealth} health.");
+
+} while (heroHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
+
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,6 +1,22 @@
-﻿string word;
+﻿/*
+===============
+E X E R C I S E
+===============
+Here are the FizzBuzz rules that you need to implement in your code project:
+- Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+- When the current value is divisible by 3, print the term Fizz next to the number.
+- When the current value is divisible by 5, print the term Buzz next to the number.
+- When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
+===============
+By MS Learn: 
+https://learn.microsoft.com/en-us/training/modules/csharp-for/3-challenge
+===============
+********/
 
-for (int i = 1; i < 101; i++)
+
+var word = "";
+
+for (var i = 1; i < 101; i++)
 {
     word = i.ToString();
     if (i % 15 == 0)
